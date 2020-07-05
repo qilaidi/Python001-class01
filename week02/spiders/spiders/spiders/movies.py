a63658e2970103dea570a53434ac12ad9c6ba943
Kernel <qilaidi@netease.com>
2020-07-05 20:51:40 +0800

    homework update catch exceptions

--- week02/spiders/spiders/spiders/movies.py
+++ week02/spiders/spiders/spiders/movies.py
@@ -16,27 +16,31 @@ class MoviesSpider(scrapy.Spider):
         yield scrapy.Request(url=self.base_url+uri, callback=self.parse)
 
     def parse(self, response):
-        movies = Selector(response).xpath('//div[@class="movie-item film-channel"]')[:10]
-        for movie in movies:
-            uri = movie.xpath("./a/@href").extract_first()
-            print(uri)
-            yield scrapy.Request(url=self.base_url+uri, callback=self.parse_details)
-
+        try:
+            movies = Selector(response).xpath('//div[@class="movie-item film-channel"]')[:10]
+            for movie in movies:
+                uri = movie.xpath("./a/@href").extract_first()
+                print(uri)
+                yield scrapy.Request(url=self.base_url+uri, callback=self.parse_details)
+        except Exception as e:
+            print(e)
 
     def parse_details(self, response):
         item = MaoYanMovieItem()
-        movie_name = Selector(response).xpath("//h1/text()").extract_first()
-        movie_type = Selector(response).xpath("//div[@class='movie-brief-container']//li[1]//a/text()").extract()
-        movie_date = re.search("\d{4}-\d{2}-\d{2}", Selector(response).xpath("//div[@class='movie-brief-container"
-                                                                             "']//li[3]/text()").extract_first(
-
-        )).group()
-        print("-"*50)
-        print(f"movie_name = {movie_name}")
-        print(f"movie_type = {movie_type}")
-        print(f"movie_date = {movie_date}")
-        print("-"*50)
-        item["movie_name"] = movie_name
-        item["movie_type"] = movie_type
-        item["movie_date"] = movie_date
+        try:
+            movie_name = Selector(response).xpath("//h1/text()").extract_first()
+            movie_type = Selector(response).xpath("//div[@class='movie-brief-container']//li[1]//a/text()").extract()
+            movie_date = re.search("\d{4}-\d{2}-\d{2}", Selector(response).xpath("//div[@class='movie-brief-container"
+                                                                                 "']//li[3]/text()").extract_first(
+            )).group()
+            print("-"*50)
+            print(f"movie_name = {movie_name}")
+            print(f"movie_type = {movie_type}")
+            print(f"movie_date = {movie_date}")
+            print("-"*50)
+            item["movie_name"] = movie_name
+            item["movie_type"] = movie_type
+            item["movie_date"] = movie_date
+        except Exception as e:
+            print(e)
         yield item
