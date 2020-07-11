0cc981012fd530a753b37fb3538da3732ddaf55f
Kernel <qilaidi@netease.com>
2020-07-11 22:51:38 +0800

    week3 update

--- week03/python_engineer_data.py
+++ week03/python_engineer_data.py
@@ -2,7 +2,20 @@ import json
 from concurrent.futures.thread import ThreadPoolExecutor
 from time import sleep
 
+import pymysql
 import requests
+
+# mysql> CREATE TABLE lagou_job(
+#     -> id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+#     -> city VARCHAR(100),
+#     -> position_name VARCHAR(100),
+#     -> salary_level VARCHAR(100));
+from pymysql import DatabaseError
+
+
+def call_agent():
+    total_data = work()
+    save_data(total_data)
 
 
 def work():
@@ -16,6 +29,33 @@ def work():
     return total_data
 
 
+def save_data(data):
+    dbinfo = {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'username': 'root',
+        'password': 'MySQL666!',
+        'db': 'scrapy_db'
+    }
+    try:
+        conn = pymysql.connect(host=dbinfo['host'],
+                               port=dbinfo['port'],
+                               user=dbinfo['username'],
+                               password=dbinfo['password'],
+                               db=dbinfo['db'])
+        cur = conn.cursor()
+        for item in data:
+            city, position_name, salary_level = item
+            sql = f"""INSERT INTO lagou_job (city, position_name, salary_level) VALUES ('{city}', '{position_name}', '{salary_level}')"""
+            cur.execute(sql)
+        cur.close()
+        conn.commit()
+    except DatabaseError as e:
+        conn.rollback()
+    finally:
+        conn.close()
+
+
 def get_data(url):
     headers = {
         "Cookie": "LGRID=20200711214951-dbab9452-a404-460c-b504-61526fa0d84a; LGSID=20200711214951-7dce6cbe-0d10-4e75-bcc3-d44d5a7b4aa4; PRE_HOST=; PRE_LAND=https%3A%2F%2Fm.lagou.com%2F; PRE_SITE=; PRE_UTM=; _ga=GA1.2.2123862506.1594475391; _gid=GA1.2.1199929341.1594475391; _ga=GA1.3.2123862506.1594475391; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1594475390; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1594475390; X_HTTP_TOKEN=669644b246cf4e3c09357449514e10057f70f9a86e; Hm_lpvt_2f04b07136aeba81b3a364fd73385ff4=1594475391; Hm_lvt_2f04b07136aeba81b3a364fd73385ff4=1594475391; JSESSIONID=ABAAABAABGJABAJE0B81011D1314364EAFF9A09AAE00B6F; user_trace_token=20180415145551-08b1713b-407a-11e8-b885-5254005c3644; LGUID=20170219212343-a2a41067-f6a6-11e6-9012-5254005c3644",
@@ -25,7 +65,7 @@ def get_data(url):
         "Accept-Language": "en-sg",
         "Accept-Encoding": "br, gzip, deflate",
         "Connection": "keep-alive"
-        }
+    }
     sleep(5)
     result = requests.get(url, headers=headers)
     content = result.text
@@ -58,9 +98,6 @@ def get_item_from_data(data):
         salary_level = item["salary"]
         result.add((city, position_name, salary_level))
     return result
-
-
-
 
 
 if __name__ == '__main__':
