0f19ef19478cab9612f80e8eb55c48481dcfaa14
Kernel <qilaidi@netease.com>
2020-08-02 20:04:43 +0800

    week 06 homework search func done

--- week06/douban/douban_movie/views.py
+++ week06/douban/douban_movie/views.py
@@ -6,8 +6,10 @@ from .models import MovieHamilton
 
 def movie_short(request):
     # 三星级以上
-    queryset = MovieHamilton.objects.all()
+    search_key = request.GET.get('search_input')
     condtions = {'stars__gt': 3}
+    if search_key:
+        condtions = {"shorts__contains": search_key}
+    queryset = MovieHamilton.objects.all()
     good_comment = queryset.filter(**condtions)
-
     return render(request, 'result.html', locals())
