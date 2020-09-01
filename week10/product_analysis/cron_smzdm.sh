db98dee35cd393b9c2e7ab7fba669a1f2512cf13
Kernel <qilaidi@netease.com>
2020-09-01 09:17:26 +0800

    update capstone project

--- week10/product_analysis/cron_smzdm.sh
+++ week10/product_analysis/cron_smzdm.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 # cron_smzdm.sh
 
+source /etc/profile
 cd /Users/cherrychan/Documents/Documents_cc/ZhangQian/Study/Python/code/mycode/Python001-class01/week10/product_analysis
 scrapy crawl smzdm
